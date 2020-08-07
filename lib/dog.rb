8b5de52ebf07fb1d3673d02f8014946679e9af4c
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-08-06 23:51:44 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,7 @@ attr_accessor :id, :name, :breed
     end 
 
     def self.create(name:, breed:)
-        dog = Dog.new(name, breed)
+        dog = Dog.new(name: name, breed: breed)
         dog.save
         dog
     end 
@@ -54,17 +54,23 @@ attr_accessor :id, :name, :breed
     def self.find_by_id(id)
         sql = "SELECT * FROM dogs WHERE id = ?"
         result = DB[:conn].execute(sql, id)[0]
-        Song.new(id: result[0], name: result[1], breed: result[2])
+        Dog.new(id: result[0], name: result[1], breed: result[2])
     end
 
-    def self.find_or_create_by
-
+    def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+        if !dog.empty?
+            dog_data = dog[0]
+            dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        else 
+            dog = self.create(name: name, breed: breed)
+        end 
+        dog
     end 
-     
 
     def self.find_by_name(name)
         sql = "SELECT * FROM dogs WHERE name = ?"
-        result = DB[:conn].execute(sql, name)[1]
+        result = DB[:conn].execute(sql, name)[0]
         Dog.new(id: result[0], name: result[1], breed: result[2])
     end 
 
